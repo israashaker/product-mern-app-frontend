d2ea4e23dc6e705b09dbba0664f827d9f29f150e
Israa <israashaker17@gmail.com>
2019-04-13 14:33:19 +0200

    edit urls

--- src/api/product.js
+++ src/api/product.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 let baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:5060";
 export const apiSaveProduct = product=>{
-    return axios.post(`${baseURL}/api/product`,product,{headers: {'Access-Control-Allow-Origin': '*'}});
+    return axios.post(`${baseURL}/api/product`,product);
 } 
 export const apiFetchProduct = (url)=>{
     return axios.get(`${baseURL}${url}`);
 }
 export const apiEditProduct = (url,content)=>{
-    return axios.put(`${baseURL}${url}`,content,{headers: {'Access-Control-Allow-Origin': '*'}});
+    return axios.put(`${baseURL}${url}`,content);
 }
 export const apiDeleteProduct = (url)=>{
-    return axios.delete(`${baseURL}${url}`,{headers: {'Access-Control-Allow-Origin': '*'}});
+    return axios.delete(`${baseURL}${url}`);
 }
