f55f349644613b1843119e578c9c1e80d780d833
Israa <israashaker17@gmail.com>
2019-04-13 14:26:44 +0200

    edit urls

--- src/api/user.js
+++ src/api/user.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 let baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:5060";
 export const apiLogin = request_data=>{
-    return axios.post(`${baseURL}/api/auth`,request_data);
+    return axios.post(`${baseURL}/api/auth`,request_data,{headers: {'Access-Control-Allow-Origin': '*'}});
 }
 export const apiSignUp = request_data=>{
-    return axios.post(`${baseURL}/api/register`,request_data);
+    return axios.post(`${baseURL}/api/register`,request_data,{headers: {'Access-Control-Allow-Origin': '*'}});
 }
 export const getProfile = ()=>{
-    return axios.get(`${baseURL}/api/me`);
+    return axios.get(`${baseURL}/api/me`,{headers: {'Access-Control-Allow-Origin': '*'}});
 }
