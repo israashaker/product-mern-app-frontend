29a9ab8e11fffe274f6e3f07c79b0f877a54697c
Israa <israashaker17@gmail.com>
2019-04-13 14:03:06 +0200

    edit urls

--- src/api/product.js
+++ src/api/product.js
@@ -1,18 +1,14 @@
 import axios from 'axios';
-//let baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:5060";
+let baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:5060";
 export const apiSaveProduct = product=>{
-    // return axios.post(`${baseURL}/api/product`,product);
-    return axios.post(`/api/product`,product);
+    return axios.post(`${baseURL}/api/product`,product);
 } 
 export const apiFetchProduct = (url)=>{
-    // return axios.get(`${baseURL}${url}`);
-    return axios.get(url);
+    return axios.get(`${baseURL}${url}`);
 }
 export const apiEditProduct = (url,content)=>{
-    // return axios.put(`${baseURL}${url}`,content);
-    return axios.put(url,content);
+    return axios.put(`${baseURL}${url}`,content);
 }
 export const apiDeleteProduct = (url)=>{
-    // return axios.delete(`${baseURL}${url}`);
-    return axios.delete(url);
+    return axios.delete(`${baseURL}${url}`);
 }
--- src/api/user.js
+++ src/api/user.js
@@ -1,14 +1,11 @@
 import axios from 'axios';
-//let baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:5060";
+let baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:5060";
 export const apiLogin = request_data=>{
-    // return axios.post(`${baseURL}/api/auth`,request_data);
-    return axios.post(`/api/auth`,request_data);
+    return axios.post(`${baseURL}/api/auth`,request_data);
 }
 export const apiSignUp = request_data=>{
-    // return axios.post(`${baseURL}/api/register`,request_data);
-       return axios.post(`/api/register`,request_data);
+    return axios.post(`${baseURL}/api/register`,request_data);
 }
 export const getProfile = ()=>{
-    // return axios.get(`${baseURL}/api/me`);
-    return axios.get(`/api/me`);
+    return axios.get(`${baseURL}/api/me`);
 }
